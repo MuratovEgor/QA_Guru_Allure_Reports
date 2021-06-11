b6541869a5f3b3b886930a371474546b3a378fd9
Egor Muratov <egormuratov@yahoo.com>
2021-06-11 16:46:40 +0300

    Created TestBase

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,11 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    public static void setUp() {
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://github.com";
+    }
+}
